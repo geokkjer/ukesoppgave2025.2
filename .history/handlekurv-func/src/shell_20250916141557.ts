c515d453465e23e7ffbd33a2e50e8672e999477b
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-09-17 11:30:12 +0200

    add DataFlowChart

---
+++ .history/handlekurv-func/src/shell_20250916141557.ts
@@ -0,0 +1,29 @@
+import type { Model } from "./types";
+import { productsView } from "./productsView";
+import { footer, navBar } from "./commonView";
+import { mainView } from "./commonView";
+
+export function render(state: Model, action: string | null): void {
+  
+  // const renderMainView = mainView();
+
+  
+  // console.log("Rendering with state:", state);
+  // console.log("Action:", action);
+
+  const oldState = {
+    ...state,
+  };
+if (oldState.app.currentPage === 'products') {
+  productsView(oldState);
+  navBar(state);
+  footer();
+} else if (oldState.app.currentPage === 'cart') {
+
+  navBar(state);
+  footer();
+  const app = document.getElementById('app');
+  app!.innerHTML = 'this is the cart page';
+}
+}
+
