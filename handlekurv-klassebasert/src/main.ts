f356050211d810df7f96891762ec9a9202375952
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-09-26 10:49:22 +0200

    klasse

--- handlekurv-klassebasert/src/main.ts
+++ handlekurv-klassebasert/src/main.ts
@@ -1,1 +1,5 @@
+import { ButtonComponent } from "./components/button";
+import { App } from "./views/App";
 
+customElements.define('my-app', App);
+customElements.define('my-button', ButtonComponent);
