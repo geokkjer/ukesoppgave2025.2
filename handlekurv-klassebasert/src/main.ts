e024d4a6f92a5c87b2d12ae3144b64e525938c60
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-09-26 10:39:16 +0200

    klasse

--- handlekurv-klassebasert/src/main.ts
+++ handlekurv-klassebasert/src/main.ts
@@ -1,11 +1,24 @@
-import { App } from "./views/App";
-import { LoginView } from "./components/loginView";
-import { Header } from "./components/header";
-import { Button } from "./components/button";
-import { Products } from "./components/products";
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
 
-customElements.define("handlekurv-app", App);
-customElements.define("login-view", LoginView);
-customElements.define("header-component", Header);
-customElements.define("custom-button", Button);
-customElements.define("products-view", Products);
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
